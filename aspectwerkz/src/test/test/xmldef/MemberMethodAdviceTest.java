71749bb7c02dd4ceb4e48290b21a29f4bc47cd59
avasseur <avasseur@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-07-22 16:20:11 +0000

    merge 3rd - only merged effectively transform package - other is version doclet pollution

--- aspectwerkz/src/test/test/xmldef/MemberMethodAdviceTest.java
+++ aspectwerkz/src/test/test/xmldef/MemberMethodAdviceTest.java
@@ -6,7 +6,7 @@ import org.codehaus.aspectwerkz.AspectWerkz;
 
 /**
  * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
- * @version $Id: MemberMethodAdviceTest.java,v 1.4.2.1 2003-07-20 10:38:38 avasseur Exp $
+ * @version $Id: MemberMethodAdviceTest.java,v 1.4.2.2 2003-07-22 16:20:11 avasseur Exp $
  */
 public class MemberMethodAdviceTest extends TestCase implements Loggable {
 
