2f42a5788b356afccf4cc73de2d4a68d093aed05
avasseur <avasseur@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-08-04 16:11:42 +0000

    initial import

---
+++ aspectwerkz/src/native/test/test/TestCallee.java
@@ -0,0 +1,16 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package test;
+
+public class TestCallee {
+
+public static void call() {
+	System.out.println("TestCallee.call()");
+}
+
+}
