f78764019d37f84eaa011a25999ac5f188b1b1c7
nobody <nobody@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-08-29 19:20:56 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.11.2'.

---
+++ aspectwerkz/src/samples/examples/xmldef/logging/LoggingAdvice.java
@@ -0,0 +1,55 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package examples.logging;
+
+import org.codehaus.aspectwerkz.advice.AroundAdvice;
+import org.codehaus.aspectwerkz.joinpoint.JoinPoint;
+import org.codehaus.aspectwerkz.joinpoint.MethodJoinPoint;
+
+/**
+ * This advice implements a simple logging service.<br/>
+ *
+ * It logs the entry A exit of the methods that are picked out
+ * by the pointcuts mapped to this advice.
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ *
+ * @aspectwerkz.advice-def name=log
+ *                         deployment-model=perJVM
+ *                         attribute=log
+ * @aspectwerkz.advice-param advice-ref=log
+ *                           name=param
+ *                           value=value
+ */
+public class LoggingAdvice extends AroundAdvice {
+
+    private int m_level = 0;
+
+    public LoggingAdvice() {
+        super();
+    }
+
+    public Object execute(final JoinPoint joinPoint) throws Throwable {
+//        System.out.println("param to advice = " + getParameter("param"));
+        MethodJoinPoint jp = (MethodJoinPoint)joinPoint;
+        indent();
+        System.out.println("--> " + jp.getTargetClass().getName() + "::" + jp.getMethodName());
+        m_level++;
+        final Object result = joinPoint.proceed();
+        m_level--;
+        indent();
+        System.out.println("<-- " + jp.getTargetClass().getName() + "::" + jp.getMethodName());
+        return result;
+    }
+
+    private void indent() {
+        for (int i = 0; i < m_level; i++) {
+            System.out.print("  ");
+        }
+    }
+}
