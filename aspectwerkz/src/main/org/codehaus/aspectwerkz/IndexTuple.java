e6574b1e393ebdd48392de190409a8ea09dfe2bd
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-09-01 20:04:01 +0000

    *** empty log message ***

---
+++ aspectwerkz/src/main/org/codehaus/aspectwerkz/IndexTuple.java
@@ -0,0 +1,55 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz;
+
+/**
+ * A tuple with two indexes describing the aspect and a method (advice or introduced).
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class IndexTuple {
+
+    /**
+     * Index for the aspect.
+     */
+    private final int m_aspectIndex;
+
+    /**
+     * Index for the advice method.
+     */
+    private final int m_methodIndex;
+
+    /**
+     * Creates a new index tuple.
+     *
+     * @param aspectIndex the aspect index
+     * @param methodIndex the method index
+     */
+    public IndexTuple(final int aspectIndex, final int methodIndex) {
+        m_aspectIndex = aspectIndex;
+        m_methodIndex = methodIndex;
+    }
+
+    /**
+     * Return the aspect index.
+     *
+     * @return the aspect index
+     */
+    public int getAspectIndex() {
+        return m_aspectIndex;
+    }
+
+    /**
+     * Return the method index.
+     *
+     * @return the method index
+     */
+    public int getMethodIndex() {
+        return m_methodIndex;
+    }
+}
