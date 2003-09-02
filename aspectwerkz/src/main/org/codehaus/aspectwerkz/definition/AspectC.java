f916f644e60bc46f9ad595c61a60c1ce940f7c52
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-09-02 22:06:47 +0000

    initial upload of attributes implementation

---
+++ aspectwerkz/src/main/org/codehaus/aspectwerkz/definition/AspectC.java
@@ -0,0 +1,229 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.definition;
+
+import com.thoughtworks.qdox.model.DocletTag;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaMethod;
+
+import org.codehaus.aspectwerkz.metadata.QDoxParser;
+import org.codehaus.aspectwerkz.definition.attribute.AspectAttribute;
+import org.codehaus.aspectwerkz.definition.attribute.PointcutAttribute;
+import org.codehaus.aspectwerkz.definition.attribute.AroundAdviceAttribute;
+import org.codehaus.aspectwerkz.definition.attribute.PreAdviceAttribute;
+import org.codehaus.aspectwerkz.definition.attribute.PostAdviceAttribute;
+import org.codehaus.aspectwerkz.definition.attribute.IntroductionAttribute;
+
+/**
+ * Compiles attributes for the aspects. Can be called from the command line.
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class AspectC {
+
+    public static final String ATTR_ASPECT = "Aspect";
+    public static final String ATTR_POINTCUT = "Pointcut";
+    public static final String ATTR_AROUND_ADVICE = "AroundAdvice";
+    public static final String ATTR_PRE_ADVICE = "PreAdvice";
+    public static final String ATTR_POST_ADVICE = "PostAdvice";
+    public static final String ATTR_INTRODUCTION = "Introduction";
+
+    /**
+     * Compiles attributes for the aspects.
+     *
+     * @param sourcePath the path to the sources to compile attributes for
+     * @param classPath the path to the compiled classes matching the source files
+     */
+    public static void compile(final String sourcePath, final String classPath) {
+        compile(sourcePath, classPath, classPath);
+    }
+
+    /**
+     * Compiles attributes for the aspects.
+     *
+     * @param sourcePath the path to the sources to compile attributes for
+     * @param classPath the path to the compiled classes matching the source files
+     * @param destDir the path where to write the compiled aspects
+     */
+    public static void compile(final String sourcePath,
+                               final String classPath,
+                               final String destDir) {
+        if (sourcePath == null) throw new IllegalArgumentException("source path can not be null");
+        if (classPath == null) throw new IllegalArgumentException("class path can not be null");
+
+        final QDoxParser qdoxParser = new QDoxParser(sourcePath);
+        String[] classNames = qdoxParser.getAllClassNames();
+
+        for (int i = 0; i < classNames.length; i++) {
+            String className = classNames[i];
+
+            AttributeEnhancer enhancer = new BcelAttributeEnhancer(); // TODO: use factory
+            enhancer.initialize(className, classPath);
+
+            if (qdoxParser.parse(className)) {
+                JavaClass javaClass = qdoxParser.getJavaClass();
+                parseAspect(javaClass, enhancer);
+
+                JavaMethod[] javaMethods = javaClass.getMethods();
+                for (int j = 0; j < javaMethods.length; j++) {
+                    JavaMethod javaMethod = javaMethods[j];
+                    parsePointcut(javaMethod, enhancer);
+                    parseAroundAdvice(javaMethod, enhancer);
+                    parsePreAdvice(javaMethod, enhancer);
+                    parsePostAdvice(javaMethod, enhancer);
+                    parseIntroduction(javaMethod, enhancer);
+                }
+            }
+            enhancer.write(destDir);
+        }
+    }
+
+    /**
+     * Parses the aspect attribute.
+     *
+     * @param javaClass the java class
+     * @param enhancer the attribute enhancer
+     */
+    private static void parseAspect(final JavaClass javaClass,
+                                    final AttributeEnhancer enhancer) {
+        DocletTag[] aspectTags = javaClass.getTagsByName(ATTR_ASPECT);
+        for (int j = 0; j < aspectTags.length; j++) {
+            String deploymentModel = aspectTags[j].getValue();
+            enhancer.insertClassAttribute(new AspectAttribute(deploymentModel));
+        }
+    }
+
+    /**
+     * Parses the pointcut attribute.
+     *
+     * @param javaMethod the java method
+     * @param enhancer the attribute enhancer
+     */
+    private static void parsePointcut(final JavaMethod javaMethod,
+                                      final AttributeEnhancer enhancer) {
+        DocletTag[] pointcutTags = javaMethod.getTagsByName(ATTR_POINTCUT);
+        StringBuffer pointcutExpr = new StringBuffer();
+        for (int k = 0; k < pointcutTags.length; k++) {
+            pointcutExpr.append(pointcutTags[k].getValue());
+        }
+        if (pointcutTags.length != 0) {
+            enhancer.insertMethodAttribute(
+                    javaMethod,
+                    new PointcutAttribute(pointcutExpr.toString())
+            );
+        }
+    }
+
+    /**
+     * Parses the around advice attribute.
+     *
+     * @param javaMethod the java method
+     * @param enhancer the attribute enhancer
+     */
+    private static void parseAroundAdvice(final JavaMethod javaMethod,
+                                          final AttributeEnhancer enhancer) {
+        DocletTag[] aroundAdviceTags = javaMethod.getTagsByName(ATTR_AROUND_ADVICE);
+        StringBuffer aroundAdviceExpr = new StringBuffer();
+        for (int k = 0; k < aroundAdviceTags.length; k++) {
+            aroundAdviceExpr.append(aroundAdviceTags[k].getValue());
+        }
+        if (aroundAdviceTags.length != 0) {
+            enhancer.insertMethodAttribute(
+                    javaMethod,
+                    new AroundAdviceAttribute(aroundAdviceExpr.toString())
+            );
+        }
+    }
+
+    /**
+     * Parses the pre advice attribute.
+     *
+     * @param javaMethod the java method
+     * @param enhancer the attribute enhancer
+     */
+    private static void parsePreAdvice(final JavaMethod javaMethod,
+                                       final AttributeEnhancer enhancer) {
+        DocletTag[] preAdviceTags = javaMethod.getTagsByName(ATTR_PRE_ADVICE);
+        StringBuffer preAdviceExpr = new StringBuffer();
+        for (int k = 0; k < preAdviceTags.length; k++) {
+            preAdviceExpr.append(preAdviceTags[k].getValue());
+        }
+        if (preAdviceTags.length != 0) {
+            enhancer.insertMethodAttribute(
+                    javaMethod,
+                    new PreAdviceAttribute(preAdviceExpr.toString())
+            );
+        }
+    }
+
+    /**
+     * Parses the post advice attribute.
+     *
+     * @param javaMethod the java method
+     * @param enhancer the attribute enhancer
+     */
+    private static void parsePostAdvice(final JavaMethod javaMethod,
+                                        final AttributeEnhancer enhancer) {
+        DocletTag[] postAdviceTags = javaMethod.getTagsByName(ATTR_POST_ADVICE);
+        StringBuffer postAdviceExpr = new StringBuffer();
+        for (int k = 0; k < postAdviceTags.length; k++) {
+            postAdviceExpr.append(postAdviceTags[k].getValue());
+        }
+        if (postAdviceTags.length != 0) {
+            enhancer.insertMethodAttribute(
+                    javaMethod,
+                    new PostAdviceAttribute(postAdviceExpr.toString())
+            );
+        }
+    }
+
+    /**
+     * Parses the introduction attribute.
+     *
+     * @param javaMethod the java method
+     * @param enhancer the attribute enhancer
+     */
+    private static void parseIntroduction(final JavaMethod javaMethod,
+                                          final AttributeEnhancer enhancer) {
+        DocletTag[] introductionTags = javaMethod.getTagsByName(ATTR_INTRODUCTION);
+        StringBuffer introductionExpr = new StringBuffer();
+        for (int k = 0; k < introductionTags.length; k++) {
+            introductionExpr.append(introductionTags[k].getValue());
+        }
+        if (introductionTags.length != 0) {
+            enhancer.insertMethodAttribute(
+                    javaMethod,
+                    new IntroductionAttribute(introductionExpr.toString())
+            );
+        }
+    }
+
+    /**
+     * Runs the compiler from the command line.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("AspectWerkz (c) 2002-2003 The AspectWerkz Team");
+            System.out.println("usage: java [options...] org.codehaus.aspectwerkz.definition.AspectC <path to src dir> <path to classes dir> <path to destination dir>");
+            System.out.println("       <path to destination dir> is optional, if omitted the compiled aspects will be written to the initial directory");
+            System.exit(0);
+        }
+        System.out.println("compiling aspects...");
+        if (args.length == 2) {
+            AspectC.compile(args[0], args[1]);
+            System.out.println("compiled aspects written to " + args[1]);
+        }
+        else {
+            AspectC.compile(args[0], args[1], args[2]);
+            System.out.println("compiled aspects written to " + args[2]);
+        }
+        System.out.println("compilation successful");
+    }
+}
---
+++ aspectwerkz/src/main/org/codehaus/aspectwerkz/definition/BcelAttributeEnhancer.java
@@ -0,0 +1,220 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.definition;
+
+import java.util.Arrays;
+import java.net.URLClassLoader;
+import java.net.URL;
+import java.io.File;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.DataOutputStream;
+
+import com.thoughtworks.qdox.model.JavaMethod;
+
+import org.apache.bcel.classfile.Attribute;
+import org.apache.bcel.classfile.Unknown;
+import org.apache.bcel.classfile.Field;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.generic.FieldGen;
+import org.apache.bcel.generic.MethodGen;
+import org.apache.bcel.generic.ClassGen;
+import org.apache.bcel.generic.ConstantPoolGen;
+
+import org.codehaus.aspectwerkz.exception.WrappedRuntimeException;
+import attrib4j.Log;
+
+/**
+ * Enhances aspect classes with attributes.
+ * Implementation based on BCEL.
+ * Based on code from the Attrib4j project by Mark Pollack.
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class BcelAttributeEnhancer implements AttributeEnhancer {
+
+    /**
+     * The BCEL java class.
+     */
+    private JavaClass m_javaClass = null;
+
+    /**
+     * The BCEL class gen.
+     */
+    private ClassGen m_classGen = null;
+
+    /**
+     * The BCEL constant pool gen.
+     */
+    private ConstantPoolGen m_constantPoolGen = null;
+
+    /**
+     * Initializes the attribute enhancer.
+     * Must always be called before use.
+     *
+     * @param className the class name
+     * @param classPath the class path
+     */
+    public void initialize(final String className, final String classPath) {
+        try {
+            URL[] urls = new URL[]{new File(classPath).toURL()};
+            URLClassLoader loader = new URLClassLoader(urls);
+
+            String classFileName = className.replace('.', '/') + ".class";
+            InputStream classAsStream = loader.getResourceAsStream(classFileName);
+
+            ClassParser classParser = new ClassParser(classAsStream, className);
+            m_javaClass = classParser.parse();
+
+            m_constantPoolGen = new ConstantPoolGen(m_javaClass.getConstantPool());
+            m_classGen = new ClassGen(m_javaClass);
+        }
+        catch (Exception e) {
+            throw new WrappedRuntimeException(e);
+        }
+    }
+
+    /**
+     * Inserts an attribute on class level.
+     *
+     * @param attribute the attribute
+     */
+    public void insertClassAttribute(final Object attribute) {
+        if (m_classGen == null) throw new IllegalStateException("attribute enhancer is not initialized");
+        byte[] serializedAttribute = serialize(attribute);
+        Attribute attr = new Unknown(
+                m_constantPoolGen.addUtf8("Custom"),
+                serializedAttribute.length,
+                serializedAttribute,
+                m_constantPoolGen.getConstantPool()
+        );
+        m_classGen.addAttribute(attr);
+    }
+
+    /**
+     * Inserts an attribute on field level.
+     *
+     * @param field the field name
+     * @param attribute the attribute
+     */
+    public void insertFieldAttribute(final String field, final Object attribute) {
+        if (m_classGen == null) throw new IllegalStateException("attribute enhancer is not initialized");
+        byte[] serializedAttribute = serialize(attribute);
+        Field[] classfileField = m_classGen.getFields();
+        for (int i = 0; i < classfileField.length; i++) {
+            if (classfileField[i].getName().equals(field)) {
+                FieldGen fieldGen = new FieldGen(classfileField[i], m_constantPoolGen);
+                Attribute attr = new Unknown(
+                        m_constantPoolGen.addUtf8("Custom"),
+                        serializedAttribute.length,
+                        serializedAttribute,
+                        m_constantPoolGen.getConstantPool()
+                );
+                fieldGen.addAttribute(attr);
+                Field newField = fieldGen.getField();
+                m_classGen.replaceField(classfileField[i], newField);
+            }
+        }
+    }
+
+    /**
+     * Inserts an attribute on method level.
+     *
+     * @param method the QDox java method
+     * @param attribute the attribute
+     */
+    public void insertMethodAttribute(final JavaMethod method, final Object attribute) {
+        if (m_classGen == null) throw new IllegalStateException("attribute enhancer is not initialized");
+        byte[] serializedAttribute = serialize(attribute);
+        String[] methodParamTypes = new String[method.getParameters().length];
+        for (int i = 0; i < methodParamTypes.length; i++) {
+            methodParamTypes[i] = method.getParameters()[i].getType().getValue();
+        }
+        Method[] classfileMethod = m_classGen.getMethods();
+        for (int i = 0; i < classfileMethod.length; i++) {
+            if (classfileMethod[i].getName().equals(method.getName())) {
+                if (Arrays.equals(
+                        methodParamTypes,
+                        DescriptorUtil.convertToJavaFormat(classfileMethod[i].getSignature()))
+                ) {
+                    MethodGen methodGen = new MethodGen(
+                            classfileMethod[i],
+                            m_javaClass.getClassName(),
+                            m_constantPoolGen
+                    );
+
+                    Attribute attr = new Unknown(
+                            m_constantPoolGen.addUtf8("Custom"),
+                            serializedAttribute.length,
+                            serializedAttribute,
+                            m_constantPoolGen.getConstantPool()
+                    );
+                    methodGen.addAttribute(attr);
+                    Method newMethod = methodGen.getMethod();
+                    m_classGen.replaceMethod(classfileMethod[i], newMethod);
+                }
+            }
+        }
+    }
+
+    /**
+     * Writes the enhanced class to file.
+     *
+     * @param destDir the destination directory
+     */
+    public void write(final String destDir) {
+        try {
+            m_classGen.setConstantPool(m_constantPoolGen);
+            JavaClass klass = m_classGen.getJavaClass();
+
+            String path = destDir + "/" + klass.getClassName().replace('.', '/') + ".class";
+            File file = new File(path);
+            File parentFile = file.getParentFile();
+
+            boolean exists = parentFile.exists();
+            if (!exists) {
+                // directory does not exist create all directories in the path
+                boolean success = parentFile.mkdirs();
+                if (!success) {
+                    throw new RuntimeException("could not create dir structure needed to write file " + path + " to disk");
+                }
+            }
+
+            FileOutputStream fout = new FileOutputStream(path);
+            DataOutputStream out = new DataOutputStream(fout);
+            klass.dump(out);
+            fout.close();
+        }
+        catch (IOException e) {
+            throw new WrappedRuntimeException(e);
+        }
+    }
+
+    /**
+     * Serializes the attribute to byte array.
+     *
+     * @param attribute the attribute
+     * @return the attribute as a byte array
+     */
+    public static byte[] serialize(final Object attribute) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(attribute);
+            return baos.toByteArray();
+        }
+        catch (IOException e) {
+            throw new WrappedRuntimeException(e);
+        }
+    }
+}
