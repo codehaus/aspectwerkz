7c4332ba09f835663e85fc7d8f697149f3254d6e
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-09-02 08:13:02 +0000

    removed def1 examples and added a def2

--- aspectwerkz/src/samples/examples/xmldef/logging/Target.java
+++ aspectwerkz/src/samples/examples/xmldef/logging/Target.java
@@ -12,42 +12,18 @@ package examples.logging;
  */
 public class Target {
 
-    public Target() {
-        this(1);
-    }
-
-    public Target(int i) {
-    }
-
-    public Target(String i) {
-    }
-
-    /**
-     * @aspectwerkz.joinpoint.controller examples.logging.DummyJoinPointController
-     * @aspectwerkz.advice.method log
-     * @aspectwerkz.advice.method log
-     */
     public static void toLog1() {
         new Target().toLog2("parameter");
     }
 
-    /**
-     * @aspectwerkz.advice.method log
-     */
     private void toLog2(java.lang.String arg) {
         new Target().toLog3();
     }
 
-    /**
-     * @aspectwerkz.advice.method log
-     */
     private String toLog3() {
         return "result";
     }
 
-    /**
-     * @aspectwerkz.advice.method log
-     */
     public static void main(String[] args) {
         Target.toLog1();
     }
