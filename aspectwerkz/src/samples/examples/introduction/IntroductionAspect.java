7a525f86779bce8cd040520b5ae744f645a185ae
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-09-05 11:32:09 +0000

    initial upload

---
+++ aspectwerkz/src/samples/examples/introduction/IntroductionAspect.java
@@ -0,0 +1,45 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package examples.introduction;
+
+import org.codehaus.aspectwerkz.aspect.AbstractAspect;
+import org.codehaus.aspectwerkz.pointcut.Pointcut;
+
+/**
+ * @Aspect perJVM
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class IntroductionAspect extends AbstractAspect {
+
+    // ============ Pointcuts ============
+
+    /**
+     * @Pointcut class(examples.introduction.Target)
+     */
+    void classes() {}
+
+    /**
+     * @Class examples.introduction.Target
+     */
+    Pointcut classes;
+
+    // ============ Introductions ============
+
+    /**
+     * @Implements classes
+     */
+     examples.introduction.Mixin mixin;
+
+    /**
+     * @Introduction classes
+     */
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
