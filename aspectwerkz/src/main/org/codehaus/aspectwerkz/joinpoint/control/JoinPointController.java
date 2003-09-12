ac6244b20f1094f1913d702340a2c97bca6a3a87
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-09-12 13:19:31 +0000

    updated common control package

---
+++ aspectwerkz/src/main/org/codehaus/aspectwerkz/joinpoint/control/JoinPointController.java
@@ -0,0 +1,40 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.joinpoint.control;
+
+import java.io.Serializable;
+
+import org.codehaus.aspectwerkz.joinpoint.MethodJoinPoint;
+
+/**
+ * Interface to be implemented by each join point controller.
+ *
+ * @author <a href="mailto:stefan.finkenzeller@gmx.net">Stefan Finkenzeller</a>
+ */
+public interface JoinPointController extends Serializable {
+
+    /**
+     * Proceeds in the execution model for the join point to the next logical pointcut/advice
+     * <p/>
+     *
+     * Joinpoint controller implementations need to implement the business logic for handling e.g.
+     * advice redundancy, advice dependency, advice compatibility or special exception handling
+     * here.
+     *
+     * @param jp    The joinpoint using this controller
+     * @return      The result of the invocation.
+     */
+    public Object proceed(MethodJoinPoint jp) throws Throwable;
+
+    /**
+     * Clones the controller
+     *
+     * @return      Clone of this controller.
+     */
+    public JoinPointController deepCopy();
+}
