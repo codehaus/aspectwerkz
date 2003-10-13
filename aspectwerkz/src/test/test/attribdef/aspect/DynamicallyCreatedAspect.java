816f4a3f7dcb6e6a4d259a36d2bded1cfbc24c8a
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-10-13 18:27:40 +0000

    initial upload

---
+++ aspectwerkz/src/test/test/attribdef/aspect/DynamicallyCreatedAspect.java
@@ -0,0 +1,40 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package test.attribdef.aspect;
+
+import org.codehaus.aspectwerkz.attribdef.Pointcut;
+import org.codehaus.aspectwerkz.attribdef.aspect.Aspect;
+import org.codehaus.aspectwerkz.joinpoint.JoinPoint;
+import org.codehaus.aspectwerkz.joinpoint.MethodJoinPoint;
+import test.attribdef.Loggable;
+
+/**
+ * @Aspect perJVM
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class DynamicallyCreatedAspect extends Aspect {
+
+    // ============ Pointcuts ============
+
+    /** @Execution * test.attribdef.DynamicDeploymentTest.createAspectTestMethod(..) */
+    Pointcut pc1;
+
+    // ============ Advices ============
+
+    /**
+     * @Around pc1
+     */
+    public Object advice1(final JoinPoint joinPoint) throws Throwable {
+        MethodJoinPoint jp = (MethodJoinPoint)joinPoint;
+        ((Loggable)jp.getTargetInstance()).log("before1 ");
+        final Object result = joinPoint.proceed();
+        ((Loggable)jp.getTargetInstance()).log("after1 ");
+        return result;
+    }
+}
