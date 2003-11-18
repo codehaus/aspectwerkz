a68304129a67ace4374b5d7400f1f481478c4677
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-11-18 22:35:14 +0000

    added new abstraction: bind rules

---
+++ aspectwerkz/src/main/org/codehaus/aspectwerkz/xmldef/definition/BindRule.java
@@ -0,0 +1,18 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.xmldef.definition;
+
+import java.io.Serializable;
+
+/**
+ * Interface for the different weaving rules in the definition.
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public interface BindRule extends Serializable {
+}
