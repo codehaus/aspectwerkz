36828a1ada322e08e8388a7736b4c3cb88b73cde
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2003-11-18 22:28:09 +0000

    added support for custom attributes

---
+++ aspectwerkz/src/main/org/codehaus/aspectwerkz/attribdef/definition/attribute/Attribute.java
@@ -0,0 +1,18 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.attribdef.definition.attribute;
+
+import java.io.Serializable;
+
+/**
+ * Marker interface for the attribute implementations.
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public interface Attribute extends Serializable {
+}
---
+++ aspectwerkz/src/main/org/codehaus/aspectwerkz/attribdef/definition/attribute/CustomAttribute.java
@@ -0,0 +1,78 @@
+/**************************************************************************************
+ * Copyright (c) The AspectWerkz Team. All rights reserved.                           *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.attribdef.definition.attribute;
+
+/**
+ * Attribute for the generic attributes defined by the user.
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class CustomAttribute implements Attribute {
+
+    /**
+     * @TODO: calculate serialVersionUID
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The name of the attribute.
+     */
+    private final String m_name;
+
+    /**
+     * The value of the attribute.
+     */
+    private final String m_value;
+
+    /**
+     * The the parameters passed to the attribute.
+     */
+//    private final String[] m_parameters;
+
+    /**
+     * Create a new CustomAttribute instance.
+     *
+     * @param name the name
+     * @param value the value
+     * @param parameters the parameters
+     */
+    public CustomAttribute(final String name, final String value, final String[] parameters) {
+        m_name = name;
+        m_value = value;
+
+        // TODO: put the parameters pairs in a hashmap to allow easy retrieval
+//        m_parameters = parameters;
+    }
+
+    /**
+     * Returns the name of the attribute.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return m_name;
+    }
+
+    /**
+     * Returns the value of the attribute.
+     *
+     * @return the value
+     */
+    public String getValue() {
+        return m_value;
+    }
+
+    /**
+     * Returns the parameters.
+     *
+     * @return the parameters
+     */
+//    public String[] getParameters() {
+//        return m_parameters;
+//    }
+}
