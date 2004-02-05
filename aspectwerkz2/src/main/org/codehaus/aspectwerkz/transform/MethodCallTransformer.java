3938c0986b781bbdeadf9992a75fae3b05df8561
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-02-05 21:33:06 +0000

    new transformers for preparation, method call and method execution

---
+++ aspectwerkz2/src/main/org/codehaus/aspectwerkz/transform/MethodCallTransformer.java
@@ -0,0 +1,258 @@
+/**************************************************************************************
+ * Copyright (c) Jonas Bonér, Alexandre Vasseur. All rights reserved.                 *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the QPL license       *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.transform;
+
+import java.util.List;
+import java.util.Iterator;
+
+import javassist.CtClass;
+import javassist.Modifier;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+import javassist.NotFoundException;
+import javassist.CtField;
+import javassist.CtMethod;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+
+import org.codehaus.aspectwerkz.metadata.MethodMetaData;
+import org.codehaus.aspectwerkz.metadata.ClassMetaData;
+import org.codehaus.aspectwerkz.metadata.JavassistMetaDataMaker;
+import org.codehaus.aspectwerkz.exception.WrappedRuntimeException;
+import org.codehaus.aspectwerkz.definition.SystemDefinition;
+import org.codehaus.aspectwerkz.definition.DefinitionLoader;
+import org.codehaus.aspectwerkz.transform.TransformationUtil;
+
+/**
+ *
+ * @author <a href="mailto:alex@gnilux.com">Alexandre Vasseur</a>
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class MethodCallTransformer implements Transformer {
+
+    /**
+     * List with the definitions.
+     */
+    private List m_definitions;
+
+    /**
+     * Creates a new instance of the transformer.
+     */
+    public MethodCallTransformer() {
+        m_definitions = DefinitionLoader.getDefinitions();
+    }
+
+    /**
+     * Transforms the call side pointcuts.
+     *
+     * @param context the transformation context
+     * @param klass the class set.
+     */
+    public void transform(final Context context, final Klass klass) throws NotFoundException, CannotCompileException {
+
+        // loop over all the definitions
+        for (Iterator it = m_definitions.iterator(); it.hasNext();) {
+            final SystemDefinition definition = (SystemDefinition)it.next();
+
+            final CtClass ctClass = klass.getCtClass();
+            ClassMetaData classMetaData = JavassistMetaDataMaker.createClassMetaData(ctClass);
+
+            // filter caller classes
+            if (classFilter(definition, classMetaData, ctClass)) {
+                return;
+            }
+
+            ctClass.instrument(new ExprEditor() {
+                public void edit(MethodCall methodCall) throws CannotCompileException {
+                    try {
+                        final CtBehavior callerBehaviour = methodCall.where();
+
+                        // filter caller methods
+                        if (methodFilterCaller(callerBehaviour)) {
+                            return;
+                        }
+
+                        // get the callee method name, signature and class name
+                        String calleeMethodName = methodCall.getMethodName();
+                        String calleeClassName = methodCall.getClassName();
+
+                        // filter callee classes
+                        if (!definition.inIncludePackage(calleeClassName)) {
+                            return;
+                        }
+                        // filter callee methods
+                        if (methodFilterCallee(calleeMethodName)) {
+                            return;
+                        }
+
+                        // create the class meta-data
+                        ClassMetaData calleeSideClassMetaData;
+                        try {
+                            calleeSideClassMetaData = JavassistMetaDataMaker.createClassMetaData(
+                                    context.getClassPool().get(calleeClassName)
+                            );
+                        }
+                        catch (NotFoundException e) {
+                            throw new WrappedRuntimeException(e);
+                        }
+
+                        // create the method meta-data
+                        MethodMetaData calleeSideMethodMetaData = JavassistMetaDataMaker.createMethodMetaData(
+                                methodCall.getMethod()
+                        );
+
+                        // is this a caller side method pointcut?
+                        if (definition.isPickedOutByCallPointcut(calleeSideClassMetaData, calleeSideMethodMetaData)) {
+
+//                            // TODO: should this caller data be passed to the join point? It is possible.
+//                            String callerMethodName = callerBehaviour.getName();
+//                            String callerMethodSignature = callerBehaviour.getSignature();
+//                            CtClass[] callerMethodParameterTypes = callerBehaviour.getParameterTypes();
+//                            int callerMethodModifiers = callerBehaviour.getModifiers();
+
+                            // add a class field for the declaring class
+                            addCalleeMethodDeclaringClassField(ctClass, methodCall.getMethod());
+
+                            // call the wrapper method instead of the callee method
+                            StringBuffer body = new StringBuffer();
+                            body.append("{ $_ = ($r)");
+                            body.append(TransformationUtil.JOIN_POINT_MANAGER_FIELD);
+                            body.append('.');
+                            body.append(TransformationUtil.PROCEED_WITH_CALL_JOIN_POINT_METHOD);
+                            body.append('(');
+                            body.append(TransformationUtil.calculateHash(methodCall.getMethod()));
+                            body.append(',');
+                            body.append("$args");
+                            body.append(',');
+                            body.append("$0");
+                            body.append(",(Class)");
+                            body.append(
+                                    TransformationUtil.STATIC_CLASS_FIELD +
+                                    TransformationUtil.DELIMITER +
+                                    methodCall.getMethod().getDeclaringClass().getName().replace('.', '_')
+                            );
+                            body.append(',');
+                            body.append(TransformationUtil.JOIN_POINT_TYPE_METHOD_CALL);
+                            body.append(",\"");
+                            body.append(methodCall.getMethod().getSignature());
+                            body.append("\"); }");
+
+                            methodCall.replace(body.toString());
+                            context.markAsAdvised();
+                        }
+                    }
+                    catch (NotFoundException nfe) {
+                        nfe.printStackTrace();
+                        // TODO: should we swallow this exception?
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Creates a new static class field, for the declaring class of the callee method.
+     *
+     * @param ctClass the class
+     */
+    private void addCalleeMethodDeclaringClassField(final CtClass ctClass, final CtMethod ctMethod)
+            throws NotFoundException, CannotCompileException {
+
+        String fieldName = TransformationUtil.STATIC_CLASS_FIELD +
+                TransformationUtil.DELIMITER +
+                ctMethod.getDeclaringClass().getName().replace('.', '_');
+
+        boolean hasField = false;
+        CtField[] fields = ctClass.getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            CtField field = fields[i];
+            if (field.getName().equals(fieldName)) {
+                hasField = true;
+                break;
+            }
+        }
+
+        if (!hasField) {
+            CtField field = new CtField(
+                    ctClass.getClassPool().get("java.lang.Class"),
+                    fieldName,
+                    ctClass
+            );
+            field.setModifiers(Modifier.STATIC | Modifier.PRIVATE | Modifier.FINAL);
+            ctClass.addField(field, "java.lang.Class.forName(\"" + ctMethod.getDeclaringClass().getName() + "\")");
+        }
+    }
+
+    /**
+     * Filters the classes to be transformed.
+     *
+     * @param definition
+     * @param classMetaData the meta-data for the class
+     * @param cg the class to filter
+     * @return boolean true if the method should be filtered away
+     */
+    private boolean classFilter(final SystemDefinition definition,
+                                final ClassMetaData classMetaData,
+                                final CtClass cg) {
+        if (cg.isInterface() || TransformationUtil.hasSuperClass(classMetaData, "org.codehaus.aspectwerkz.aspect.Aspect")) {
+            return true;
+        }
+        String className = cg.getName();
+        if (definition.inExcludePackage(className)) {
+            return true;
+        }
+        if (!definition.inIncludePackage(className)) {
+            return true;
+        }
+        if (definition.hasCallPointcut(classMetaData)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Filters the caller methods.
+     *
+     * @param method the method to filter
+     * @return boolean true if the method should be filtered away
+     */
+    private boolean methodFilterCaller(final CtBehavior method) {
+        if (Modifier.isNative(method.getModifiers()) ||
+                Modifier.isInterface(method.getModifiers()) ||
+                method.getName().equals(TransformationUtil.GET_META_DATA_METHOD) ||
+                method.getName().equals(TransformationUtil.SET_META_DATA_METHOD) ||
+                method.getName().equals(TransformationUtil.CLASS_LOOKUP_METHOD) ||
+                method.getName().equals(TransformationUtil.GET_UUID_METHOD)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Filters the callee methods.
+     *
+     * @param methodName the name of method to filter
+     * @return boolean true if the method should be filtered away
+     */
+    private boolean methodFilterCallee(final String methodName) {
+        if (methodName.equals("<init>") ||
+                methodName.equals("<clinit>") ||
+                methodName.startsWith(TransformationUtil.ORIGINAL_METHOD_PREFIX) ||
+                methodName.equals(TransformationUtil.GET_META_DATA_METHOD) ||
+                methodName.equals(TransformationUtil.SET_META_DATA_METHOD) ||
+                methodName.equals(TransformationUtil.CLASS_LOOKUP_METHOD) ||
+                methodName.equals(TransformationUtil.GET_UUID_METHOD)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ aspectwerkz2/src/main/org/codehaus/aspectwerkz/transform/MethodExecutionTransformer.java
@@ -0,0 +1,315 @@
+/**************************************************************************************
+ * Copyright (c) Jonas Bonér, Alexandre Vasseur. All rights reserved.                 *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the QPL license       *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.transform;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javassist.CtClass;
+import javassist.NotFoundException;
+import javassist.CtMethod;
+import javassist.Modifier;
+import javassist.CannotCompileException;
+import javassist.CtNewMethod;
+
+import org.codehaus.aspectwerkz.metadata.MethodMetaData;
+import org.codehaus.aspectwerkz.metadata.ClassMetaData;
+import org.codehaus.aspectwerkz.metadata.JavassistMetaDataMaker;
+import org.codehaus.aspectwerkz.definition.SystemDefinition;
+import org.codehaus.aspectwerkz.definition.DefinitionLoader;
+import org.codehaus.aspectwerkz.transform.TransformationUtil;
+
+/**
+ *
+ * @author <a href="mailto:alex@gnilux.com">Alexandre Vasseur</a>
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class MethodExecutionTransformer implements Transformer {
+
+    /**
+     * List with the definitions.
+     */
+    private List m_definitions;
+
+    /**
+     * Creates a new instance of the transformer.
+     */
+    public MethodExecutionTransformer() {
+        m_definitions = DefinitionLoader.getDefinitions();
+    }
+
+    /**
+     * Makes the member method transformations.
+     *
+     *  @TODO: track if we are in a static context and if so, pass in null instead of 'this'
+     *
+     * @param context the transformation context
+     * @param klass the class set.
+     */
+    public void transform(final Context context, final Klass klass) throws Exception {
+
+        // loop over all the definitions
+        for (Iterator it = m_definitions.iterator(); it.hasNext();) {
+            SystemDefinition definition = (SystemDefinition)it.next();
+
+            final CtClass ctClass = klass.getCtClass();
+            ClassMetaData classMetaData = JavassistMetaDataMaker.createClassMetaData(ctClass);
+
+            if (classFilter(definition, classMetaData, ctClass, false)) {
+                return;
+            }
+
+            final CtMethod[] methods = ctClass.getDeclaredMethods();
+
+            // build and sort the method lookup list
+            final List methodLookupList = new ArrayList();
+            for (int i = 0; i < methods.length; i++) {
+                MethodMetaData methodMetaData = JavassistMetaDataMaker.createMethodMetaData(methods[i]);
+                if (methodFilter(definition, classMetaData, methodMetaData, methods[i])) {
+                    continue;
+                }
+                if (methodInternal(methods[i])) {
+                    continue;
+                }
+                methodLookupList.add(methods[i]);
+            }
+
+            final Map methodSequences = new HashMap();
+            final List wrapperMethods = new ArrayList();
+            boolean isClassAdvised = false;
+            for (Iterator i = methodLookupList.iterator(); i.hasNext();) {
+                CtMethod method = (CtMethod)i.next();
+                MethodMetaData methodMetaData = JavassistMetaDataMaker.createMethodMetaData(method);
+
+                if (methodFilter(definition, classMetaData, methodMetaData, method)) {
+                    continue;
+                }
+
+                isClassAdvised = true;
+
+                // take care of identification of overloaded methods by inserting a sequence number
+                if (methodSequences.containsKey(method.getName())) {
+                    int sequence = ((Integer)methodSequences.get(method.getName())).intValue();
+                    methodSequences.remove(method.getName());
+                    sequence++;
+                    methodSequences.put(method.getName(), new Integer(sequence));
+                }
+                else {
+                    methodSequences.put(method.getName(), new Integer(1));
+                }
+
+                final int methodSequence = ((Integer)methodSequences.get(method.getName())).intValue();
+                final int methodHash = TransformationUtil.calculateHash(method);
+
+                CtMethod wrapperMethod = createWrapperMethod(ctClass, method, methodHash);
+                wrapperMethods.add(wrapperMethod);
+
+                addPrefixToMethod(ctClass, method, methodSequence);
+            }
+
+            if (isClassAdvised) {
+                context.markAsAdvised();
+
+                // add the wrapper methods
+                for (Iterator it2 = wrapperMethods.iterator(); it2.hasNext();) {
+                    ctClass.addMethod((CtMethod)it2.next());
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a wrapper method for the original method specified.
+     * This method has the same signature as the original method and
+     * catches the invocation for further processing by the framework
+     * before redirecting to the original method.
+     *
+     * Genereates code similar to this:
+     * <pre>
+     *        return (ReturnType)___AW_joinPointManager.proceedWithExecutionJoinPoint(
+     *            joinPointHash, new Object[]{parameter}, this,
+     *            JoinPointType.METHOD_EXECUTION, joinPointSignature
+     *        );
+     * </pre>
+     *
+     * @param ctClass the ClassGen
+     * @param originalMethod the current method
+     * @param methodHash the method hash
+     * @return the wrapper method
+     */
+    private CtMethod createWrapperMethod(final CtClass ctClass,
+                                         final CtMethod originalMethod,
+                                         final int methodHash)
+            throws NotFoundException, CannotCompileException {
+
+        StringBuffer body = new StringBuffer();
+        body.append("{ return ($r)");
+        body.append(TransformationUtil.JOIN_POINT_MANAGER_FIELD);
+        body.append('.');
+        body.append(TransformationUtil.PROCEED_WITH_EXECUTION_JOIN_POINT_METHOD);
+        body.append('(');
+        body.append(methodHash);
+        body.append(", ");
+        body.append("$args");
+        body.append(", ");
+        if (Modifier.isStatic(originalMethod.getModifiers())) {
+            body.append("(Object)null");
+        }
+        else {
+            body.append("this");
+        }
+        body.append(',');
+        body.append(TransformationUtil.JOIN_POINT_TYPE_METHOD_EXECUTION);
+        body.append(",\"");
+        body.append(originalMethod.getSignature());
+        body.append("\"); }");
+
+        CtMethod method = null;
+        if (Modifier.isStatic(originalMethod.getModifiers())) {
+            method = JavassistHelper.makeStatic(
+                    originalMethod.getReturnType(),
+                    originalMethod.getName(),
+                    originalMethod.getParameterTypes(),
+                    originalMethod.getExceptionTypes(),
+                    body.toString(),
+                    ctClass
+            );
+        }
+        else {
+            method = CtNewMethod.make(
+                    originalMethod.getReturnType(),
+                    originalMethod.getName(),
+                    originalMethod.getParameterTypes(),
+                    originalMethod.getExceptionTypes(),
+                    body.toString(),
+                    ctClass
+            );
+            method.setModifiers(originalMethod.getModifiers());
+        }
+
+        return method;
+    }
+
+    /**
+     * Adds a prefix to the original method.
+     * To make it callable only from within the framework itself.
+     *
+     * @param cg class gen
+     * @param ctMethod the current method
+     * @param methodSequence the methods sequence number
+     */
+    private void addPrefixToMethod(final CtClass cg, final CtMethod ctMethod, final int methodSequence) {
+        // change the method access flags (should always be set to protected)
+        int accessFlags = ctMethod.getModifiers();
+        if ((accessFlags & Modifier.PROTECTED) == 0) {
+            // set the protected flag
+            accessFlags |= Modifier.PROTECTED;
+        }
+        if ((accessFlags & Modifier.PRIVATE) != 0) {
+            // clear the private flag
+            accessFlags &= ~Modifier.PRIVATE;
+        }
+        if ((accessFlags & Modifier.PUBLIC) != 0) {
+            // clear the public flag
+            accessFlags &= ~Modifier.PUBLIC;
+        }
+
+        String prefixedMethodName = TransformationUtil.getPrefixedMethodName(ctMethod, methodSequence, cg.getName());
+        ctMethod.setName(prefixedMethodName);
+        ctMethod.setModifiers(accessFlags);
+    }
+
+    /**
+     * Filters the classes to be transformed.
+     *
+     * @param definition the definition
+     * @param classMetaData the meta-data for the class
+     * @param cg the class to filter
+     * @return boolean true if the method should be filtered away
+     */
+    private boolean classFilter(final SystemDefinition definition,
+                                final ClassMetaData classMetaData,
+                                final CtClass cg,
+                                final boolean isActivatePhase) {
+        if (cg.isInterface() || TransformationUtil.hasSuperClass(classMetaData, "org.codehaus.aspectwerkz.aspect.Aspect")) {
+            return true;
+        }
+        String className = cg.getName();
+        if (definition.inExcludePackage(className)) {
+            return true;
+        }
+        if (!definition.inIncludePackage(className)) {
+            return true;
+        }
+        if (definition.inPreparePackage(className) && !isActivatePhase) {
+            return true;
+        }
+        if (definition.hasExecutionPointcut(classMetaData) || definition.hasThrowsPointcut(classMetaData)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Filters the methods to be transformed.
+     *
+     * @param definition the definition
+     * @param classMetaData the class meta-data
+     * @param method the method to filter
+     * @return boolean
+     */
+    private boolean methodFilter(final SystemDefinition definition,
+                                 final ClassMetaData classMetaData,
+                                 final MethodMetaData methodMetaData,
+                                 final CtMethod method) {
+        if (Modifier.isAbstract(method.getModifiers()) ||
+                Modifier.isNative(method.getModifiers()) ||
+                method.getName().equals("<init>") ||
+                method.getName().equals("<clinit>") ||
+                method.getName().startsWith(TransformationUtil.ORIGINAL_METHOD_PREFIX) ||
+                method.getName().equals(TransformationUtil.GET_META_DATA_METHOD) ||
+                method.getName().equals(TransformationUtil.SET_META_DATA_METHOD) ||
+                method.getName().equals(TransformationUtil.CLASS_LOOKUP_METHOD) ||
+                method.getName().equals(TransformationUtil.GET_UUID_METHOD)) {
+            return true;
+        }
+        else if (definition.hasExecutionPointcut(classMetaData, methodMetaData)) {
+            return false;
+        }
+        else if (definition.hasThrowsPointcut(classMetaData, methodMetaData)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    /**
+     * Filters the internal methods
+     *
+     * @param method the method to filter
+     * @return boolean
+     */
+    private boolean methodInternal(final CtMethod method) {
+        if (Modifier.isAbstract(method.getModifiers()) ||
+                Modifier.isNative(method.getModifiers()) ||
+                method.getName().equals("<init>") ||
+                method.getName().equals("<clinit>") ||
+                method.getName().startsWith(TransformationUtil.ORIGINAL_METHOD_PREFIX) ||
+                method.getName().equals(TransformationUtil.GET_META_DATA_METHOD) ||
+                method.getName().equals(TransformationUtil.SET_META_DATA_METHOD) ||
+                method.getName().equals(TransformationUtil.CLASS_LOOKUP_METHOD) ||
+                method.getName().equals(TransformationUtil.GET_UUID_METHOD)) {
+            return true;
+        }
+        return false;
+    }
+}
