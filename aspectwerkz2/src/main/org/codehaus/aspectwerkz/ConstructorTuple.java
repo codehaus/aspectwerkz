1be583a800c22d1482adbba790367a8c77a3bb12
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-02-13 16:54:01 +0000

    initial upload

---
+++ aspectwerkz2/src/main/org/codehaus/aspectwerkz/ConstructorTuple.java
@@ -0,0 +1,60 @@
+/**************************************************************************************
+ * Copyright (c) Jonas Bonér, Alexandre Vasseur. All rights reserved.                 *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD-style license *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Contains a pair of the original method and the wrapper method if such a method exists.
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class ConstructorTuple {
+    private final Constructor m_wrapperConstructor;
+    private final Constructor m_originalConstructor;
+    private final Class m_declaringClass;
+
+    /**
+     *
+     * @param wrapperConstructor
+     * @param originalConstructor
+     */
+    public ConstructorTuple(Constructor wrapperConstructor, Constructor originalConstructor) {
+        if (originalConstructor == null) {
+            originalConstructor = wrapperConstructor;
+        }
+        if (wrapperConstructor.getDeclaringClass() != originalConstructor.getDeclaringClass()) {
+            throw new RuntimeException(wrapperConstructor.getName() + " and " + originalConstructor.getName() + " does not have the same declaring class");
+        }
+        m_declaringClass = wrapperConstructor.getDeclaringClass();
+        m_wrapperConstructor = wrapperConstructor;
+        m_wrapperConstructor.setAccessible(true);
+        m_originalConstructor = originalConstructor;
+        m_originalConstructor.setAccessible(true);
+    }
+
+    public boolean isWrapped() {
+        return m_originalConstructor != null;
+    }
+
+    public Class getDeclaringClass() {
+        return m_declaringClass;
+    }
+
+    public Constructor getWrapperConstructor() {
+        return m_wrapperConstructor;
+    }
+
+    public Constructor getOriginalConstructor() {
+        return m_originalConstructor;
+    }
+
+    public String getName() {
+        return m_wrapperConstructor.getName();
+    }
+}
