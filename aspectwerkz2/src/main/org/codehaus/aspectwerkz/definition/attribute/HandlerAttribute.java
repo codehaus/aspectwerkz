8c8179f2972f02547f85b14dc9b4da6df90cc1f5
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-02-20 16:58:25 +0000

    adding handler specific stuff

---
+++ aspectwerkz2/src/main/org/codehaus/aspectwerkz/definition/attribute/HandlerAttribute.java
@@ -0,0 +1,45 @@
+/**************************************************************************************
+ * Copyright (c) Jonas Bonér, Alexandre Vasseur. All rights reserved.                 *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the QPL license       *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.definition.attribute;
+
+/**
+ * Attribute for the handler pointcut.
+ *
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér</a>
+ */
+public class HandlerAttribute implements Attribute {
+
+    /**
+     * @TODO: recalculate
+     */
+    private static final long serialVersionUID = 727314474096204037L;
+
+    /**
+     * The expression for the pointcut.
+     */
+    private final String m_expression;
+
+    /**
+     * Create a handler attribute.
+     *
+     * @param expression the expression
+     */
+    public HandlerAttribute(final String expression) {
+        if (expression == null || expression.equals("")) throw new IllegalArgumentException("expression is not valid for handler pointcut");
+        m_expression = expression;
+    }
+
+    /**
+     * Return the expression for the pointcut.
+     *
+     * @return the expression for the pointcut
+     */
+    public String getExpression() {
+        return m_expression;
+    }
+}
