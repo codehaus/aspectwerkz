0ec8983a450594b318bde0255f549a1389215a95
avasseur <avasseur@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-05-31 13:05:29 +0000

    prepare 0.10 release (backport for 0.10 ** this commit is in a branch)

--- aspectwerkz2/bin/aspectwerkz.bat
+++ aspectwerkz2/bin/aspectwerkz.bat
@@ -8,7 +8,7 @@
 @REM ----------------------------------------------------------------------------------
 
 @ECHO OFF
-set ASPECTWERKZ_VERSION=0.10.RC2
+set ASPECTWERKZ_VERSION=0.10
 
 IF "%1"=="" goto error
 IF "%ASPECTWERKZ_HOME%"=="" goto error_no_aw_home
