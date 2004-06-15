96cc13e2f2eb77c86472bb93abf8bc9bba3a0b79
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-06-15 13:25:17 +0000

    test for interface subtype bug

--- aspectwerkz3/src/test/test/AllTests.java
+++ aspectwerkz3/src/test/test/AllTests.java
@@ -51,6 +51,7 @@ public class AllTests extends TestCase {
         suite.addTestSuite(test.aopc.AspectSystemTest.class);
         suite.addTestSuite(test.implementsbug.ImplementsTest.class);
         suite.addTestSuite(test.inheritedmixinbug.Target.class);
+        suite.addTestSuite(test.interfacesubtypebug.InterfaceSubtypeBug.class);
         suite.addTestSuite(test.performance.PerformanceTest.class);
 
         // TODO: deprecated until a better hot deployment model has been implemented
