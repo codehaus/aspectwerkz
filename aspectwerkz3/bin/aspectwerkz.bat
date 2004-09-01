3fcadbfeb2d22d143a3966ff2bf01902c090573b
avasseur <avasseur@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-09-01 09:44:06 +0000

    update release number to 1.0.RC1

--- aspectwerkz3/bin/aspectwerkz.bat
+++ aspectwerkz3/bin/aspectwerkz.bat
@@ -8,7 +8,7 @@
 @REM ----------------------------------------------------------------------------------
 
 @ECHO OFF
-set ASPECTWERKZ_VERSION=1.0-beta3
+set ASPECTWERKZ_VERSION=1.0.RC1
 
 IF "%1"=="" goto error
 IF "%ASPECTWERKZ_HOME%"=="" goto error_no_aw_home
