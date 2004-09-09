42a5e0279a42c9b7701605887f0042deef7d3152
avasseur <avasseur@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-09-09 07:19:45 +0000

    moved

---
+++ aspectwerkz3/src/jdk15/main/org/codehaus/aspectwerkz/util/StackTraceHelper.java
@@ -0,0 +1,58 @@
+/**************************************************************************************
+ * Copyright (c) Jonas Bonér, Alexandre Vasseur. All rights reserved.                 *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the LGPL license      *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.codehaus.aspectwerkz.util;
+
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Utility methods.
+ * 
+ * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér </a>
+ */
+public final class StackTraceHelper {
+
+    /**
+     * Removes the AspectWerkz specific elements from the stack trace.
+     *
+     * @TODO Make use of if java version is 1.4+ (use reflection)
+     *
+     * @param exception the Throwable to modify the stack trace on
+     * @param className the name of the fake origin class of the exception
+     */
+    public static void fakeStackTrace(final Throwable exception, final String className) {
+        if (exception == null) {
+            throw new IllegalArgumentException("exception can not be null");
+        }
+        if (className == null) {
+            throw new IllegalArgumentException("class name can not be null");
+        }
+
+        final List newStackTraceList = new ArrayList();
+        final StackTraceElement[] stackTrace = exception.getStackTrace();
+        int i;
+        for (i = 1; i < stackTrace.length; i++) {
+            if (stackTrace[i].getClassName().equals(className)) break;
+        }
+        for (int j = i; j < stackTrace.length; j++) {
+            newStackTraceList.add(stackTrace[j]);
+        }
+
+        final StackTraceElement[] newStackTrace =
+                new StackTraceElement[newStackTraceList.size()];
+        int k = 0;
+        for (Iterator it = newStackTraceList.iterator(); it.hasNext(); k++) {
+            final StackTraceElement element = (StackTraceElement)it.next();
+            newStackTrace[k] = element;
+        }
+        exception.setStackTrace(newStackTrace);
+    }
+
+}
