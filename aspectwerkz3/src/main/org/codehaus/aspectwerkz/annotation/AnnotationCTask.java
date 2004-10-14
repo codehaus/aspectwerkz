d6079cbf6717377c86bb8dc9cce374e0a295b95f
avasseur <avasseur@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-10-14 15:53:27 +0000

    refactored / clean up annotationC task to behave like pretty much like javac

--- aspectwerkz3/src/main/org/codehaus/aspectwerkz/annotation/AnnotationCTask.java
+++ aspectwerkz3/src/main/org/codehaus/aspectwerkz/annotation/AnnotationCTask.java
@@ -14,236 +14,242 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.MatchingTask;
+import org.apache.tools.ant.taskdefs.Copy;
 import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.types.Path;
+import org.apache.tools.ant.types.Reference;
 
 /**
  * AnnotationC Ant task.
  *
  * @author <a href='mailto:the_mindstorm@evolva.ro'>the_mindstorm(at)evolva(dot)ro</a>
+ * @author <a href="mailto:alex AT gnilux DOT com">Alexandre Vasseur</a>
  */
 public class AnnotationCTask extends Task {
-    protected Path classpath;
-    protected boolean verbose;
-    protected Path srcdir;
-    protected List filesets = new ArrayList();
-    protected File srcincludes;
-    protected Path clsdir;
-    protected File properties;
-    protected File destdir;
-    // deal with duplicated parameters
-    protected int srcParamType = SRC_NOTSET;
 
-    private static final int SRC_NOTSET = 0;
-    private static final int SRC_FILESET = 1;
-    private static final int SRC_DIR = 2;
-    private static final int SRC_DIRPATH = 3;
-    private static final int SRC_FILE = 4;
-    private static final String[] MSG = {"", "srcfileset", "srcdir", "inner src", "srcincludes"};
+    private final static String CLASS_PATTERN = "**/*.class";
 
+
+    private boolean m_verbose;
+    private String m_includePattern;
+    private Path m_classpath;
+    private Path m_src;
+    private File m_properties;
+    private File m_destdir;
+    private List m_filesets = new ArrayList();
+
+    /**
+     * properties=..
+     *
+     * @param annotationFile
+     */
+    public void setProperties(File annotationFile) {
+        m_properties = annotationFile;
+    }
+
+    /**
+     * <task verbose=..>
+     *
+     * @param isVerbose
+     */
     public void setVerbose(boolean isVerbose) {
-        this.verbose = true;
+        m_verbose = isVerbose;
     }
 
-    public void setDestdir(File destdir) {
-        this.destdir = destdir;
+    /**
+     * <task copytodest="** slash *">
+     * @param pattern
+     */
+    public void setCopytodest(String pattern) {
+        m_includePattern = pattern;
     }
+
+    /**
+     * <task destdir=..>
+     *
+     * @param destdir
+     */
+    public void setDestdir(File destdir) {
+        m_destdir = destdir;
+    }
+
+    //-- <src .., <sourcepath.. and srcdir=.. sourcepathref=..
 
     public Path createSrc() {
-        if (isSet(SRC_DIRPATH)) {
-            throw tooManyParameters(SRC_DIRPATH);
-        }
-        if (srcdir == null) {
-            srcdir = new Path(getProject());
-        }
-        return srcdir.createPath();
+        if (m_src == null)
+            m_src = new Path(getProject());
+        return m_src.createPath();
     }
 
-    public void setSrcdir(Path srcdir) {
-        if (isSet(SRC_DIR)) {
-            throw tooManyParameters(SRC_DIR);
-        }
-
-        if (this.srcdir == null) {
-            this.srcdir = srcdir;
-        } else {
-            this.srcdir.append(srcdir);
-        }
+    public void setSrcdir(Path srcDir) {
+        if (m_src == null)
+            m_src = srcDir;
+        else
+            m_src.append(srcDir);
     }
 
-    public void setSrcincludes(File srcinclude) {
-        if (isSet(SRC_FILE)) {
-            throw tooManyParameters(SRC_FILE);
-        }
-        this.srcincludes = srcinclude;
+    public void setSourcepath(Path sourcepath) {
+        if (m_src == null)
+            m_src = sourcepath;
+        else
+            m_src.append(sourcepath);
     }
 
-    public void addSrcfileset(FileSet set) {
-        if (isSet(SRC_FILESET)) {
-            throw tooManyParameters(SRC_FILESET);
-        }
-        this.filesets.add(set);
+    public Path createSourcepath() {
+        if (m_src == null)
+            m_src = new Path(getProject());
+        return m_src.createPath();
     }
 
-    public Path createCls() {
-        if (this.clsdir == null) {
-            clsdir = new Path(getProject());
-        }
-        return clsdir.createPath();
+    public void setSourcepathRef(Reference r) {
+        createSourcepath().setRefid(r);
     }
 
-    public void setClsdir(Path clsdir) {
-        if (this.clsdir == null) {
-            this.clsdir = clsdir;
-        } else {
-            this.clsdir.append(clsdir);
-        }
+
+    //--- classpath
+
+    public void setClasspath(Path classpath) {
+        if (m_classpath == null)
+            m_classpath = classpath;
+        else
+            m_classpath.append(classpath);
     }
 
-    public void setProperties(File annotationFile) {
-        this.properties = annotationFile;
+    public Path createClasspath() {
+        if (m_classpath == null)
+            m_classpath = new Path(getProject());
+        return m_classpath.createPath();
     }
+
+    public void setClasspathRef(Reference r) {
+        createClasspath().setRefid(r);
+    }
+
+    //---- fileset for source files
+
+    public void addFileset(FileSet fileset) {
+        m_filesets.add(fileset);
+    }
+
+    //-- Ant task
 
     public void execute() throws BuildException {
         try {
-            if ((this.srcdir == null || this.srcdir.size() == 0) && this.filesets.size() == 0
-                && (this.srcincludes == null || !this.srcincludes.isFile())) {
-                throw new BuildException("srcdir, srcfileset and srcincludes cannot be all empty");
+            if (m_classpath == null) {
+                throw new BuildException("No classes specified [<classpath, classpath=.. classpathref=..]");
             }
-            if (this.clsdir == null || this.clsdir.size() == 0) {
-                throw new BuildException("clsdir cannot be null");
+            if (m_destdir == null && m_classpath.list().length > 1) {
+                throw new BuildException(
+                        "When using more than one classpath directory, it is mandatory to specify [destdir=..]"
+                );
             }
-            if (this.properties == null || !this.properties.isFile()) {
-                throw new BuildException("properties is not a valid file [" + this.properties + "]");
+            if (m_filesets.size() == 0 && (m_src == null || m_src.size() == 0)) {
+                throw new BuildException("No source specified [<include, <sourcepath, srcdir=..]");
             }
-
-            boolean useDirs = (srcdir != null && srcdir.size() != 0);
-
-            String[] srcList = null;
-            switch (this.srcParamType) {
-                case SRC_DIR:
-                case SRC_DIRPATH:
-                    srcList = getDirList(this.srcdir, "srcdir");
-                    break;
-                case SRC_FILESET:
-                    srcList = getSourceFiles(this.filesets);
-                    break;
-                case SRC_FILE:
-                    srcList = getSourceFiles(this.srcincludes);
-                    break;
+            if (m_properties != null && !m_properties.exists() && !m_properties.isFile()) {
+                throw new BuildException("properties file specified but not a valid file [" + m_properties + "]");
             }
 
-            AnnotationC.compile(
-                    this.verbose,
-                    srcList,
-                    useDirs,
-                    getDirList(this.clsdir, "cls"),
-                    this.destdir == null ? null : this.destdir.getAbsolutePath(),
-                    this.properties.getAbsolutePath()
-            );
+            // compute source directory list
+            List srcDirs = getDirectories(m_src);
+            List srcFiles = getFilesetFiles(m_filesets);
+            List classpathDirs = getDirectories(m_classpath);
+
+            if (m_verbose) {
+                System.out.println("Source dir   : " + dump(srcDirs));
+                System.out.println("Source files : " + dump(srcFiles));
+                System.out.println("Classpath    : " + dump(classpathDirs));
+                System.out.println("Destdir      : " + m_destdir);
+                System.out.println("Properties   : " + m_properties);
+                System.out.println("Copytodest   : " + m_includePattern);
+            }
+
+//            AnnotationC.compile(
+//                    m_verbose,
+//                    (String[])srcDirs.toArray(new String[]{}),
+//                    (String[])srcFiles.toArray(new String[]{}),
+//                    (String[])classpathDirs.toArray(new String[]{}),
+//                    m_destdir == null ? null : m_destdir.getAbsolutePath(),
+//                    m_properties == null ? null : m_properties.getAbsolutePath()
+//            );
+
+            if (m_destdir != null) {
+                if (m_verbose) {
+                    System.out.println("Copying residual files to dest dir...");
+                }
+                copySourcesToDest();
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             throw new BuildException(e);
         }
     }
 
-    private String[] getSourceFiles(List fsList) throws BuildException {
-        // deal with the filesets
-        for (int i = 0; i < filesets.size(); i++) {
-            FileSet fs = (FileSet) filesets.get(i);
-            DirectoryScanner ds = fs.getDirectoryScanner(getProject());
-
-            File fromDir = fs.getDir(getProject());
-
-            String[] srcFiles = ds.getIncludedFiles();
-            for (int j = 0; j < srcFiles.length; j++) {
-                System.out.println(srcFiles[j]);
-            }
-        }
-
-        return null;
-    }
-
-    private String[] getSourceFiles(File file) throws BuildException {
+    private List getFilesetFiles(List filesets) throws BuildException {
         List files = new ArrayList();
-        BufferedReader reader = null;
-
-        try {
-            reader = new BufferedReader(new FileReader(file));
-
-            String line = reader.readLine();
-            File tmpFile = null;
-            while (line != null) {
-                if (line.length() > 0) {
-                    tmpFile = getProject().resolveFile(line);
-                    if (!tmpFile.isFile()) {
-                        log("file not found: [" + tmpFile + "]", Project.MSG_WARN);
-                    } else {
-                        files.add(tmpFile.getAbsolutePath());
-                    }
-                }
-                line = reader.readLine();
-            }
-        } catch (IOException ioe) {
-            throw new BuildException(
-                    "an error occured while reading from pattern file: " + file,
-                    ioe
-            );
-        } finally {
-            if (null != reader) {
-                try {
-                    reader.close();
-                } catch (IOException ioe) {
-                    //Ignore exception
-                }
+        for (Iterator iterator = filesets.iterator(); iterator.hasNext();) {
+            FileSet fileset = (FileSet) iterator.next();
+            DirectoryScanner ds = fileset.getDirectoryScanner(getProject());
+            for (int i = 0; i < ds.getIncludedFiles().length; i++) {
+                String file = ds.getIncludedFiles()[i];
+                files.add(ds.getBasedir() + File.separator + file);
             }
         }
-
-        return (String[]) files.toArray(new String[files.size()]);
+        return files;
     }
 
-    private String[] getDirList(Path path, String type) throws BuildException {
-        String[] list = path.list();
+    private List getDirectories(Path path) throws BuildException {
         List dirs = new ArrayList();
-
-        for (int i = 0; i < list.length; i++) {
-            File dir = getProject().resolveFile(list[i]);
+        if (path == null)
+            return dirs;
+        for (int i = 0; i < path.list().length; i++) {
+            File dir = getProject().resolveFile(path.list()[i]);
             if (!dir.exists()) {
-                throw new BuildException(
-                        type + " \"" + dir.getPath() + "\" does not exist!",
-                        getLocation()
-                );
+                throw new BuildException(" \"" + dir.getPath() + "\" does not exist!", getLocation());
             }
-
             dirs.add(dir.getAbsolutePath());
         }
-
-        return (String[]) dirs.toArray(new String[dirs.size()]);
+        return dirs;
     }
 
-    /**
-     * Checks the src related parameters.
-     */
-    private boolean isSet(int srcParam) {
-        if (SRC_NOTSET == this.srcParamType) { // nothing is set
-            this.srcParamType = srcParam;
-            return false;
+    private String dump(List strings) {
+        StringBuffer sb = new StringBuffer();
+        for (Iterator iterator = strings.iterator(); iterator.hasNext();) {
+            Object o = (Object) iterator.next();
+            sb.append(o.toString()).append(File.pathSeparator);
         }
-        if (SRC_DIRPATH == srcParam && SRC_DIRPATH == this.srcParamType) { // inner is allowed multiple times
-            return false;
-        }
-        return true;
+        return sb.toString();
     }
 
-    private BuildException tooManyParameters(int srcType) {
-        return new BuildException(
-                "cannot set both " + MSG[this.srcParamType] + " and "
-                + MSG[srcType]
-        );
+    private void copySourcesToDest() throws BuildException {
+        Copy copy = new Copy();
+
+        copy.setProject(getProject());
+        copy.setTodir(m_destdir);
+        copy.setOverwrite(false);
+        copy.setTaskName("copy");
+        copy.setVerbose(m_verbose);
+
+        List sourceDir = getDirectories(m_src);
+        for (Iterator iterator = sourceDir.iterator(); iterator.hasNext();) {
+            String dir = (String) iterator.next();
+            FileSet anonFs  = new FileSet();
+            anonFs.setIncludes(CLASS_PATTERN);
+            if (m_includePattern != null) {
+                anonFs.setIncludes(m_includePattern);
+            }
+            anonFs.setDir(new File(dir));
+            copy.addFileset(anonFs);
+        }
+        copy.execute();
     }
+
 }
