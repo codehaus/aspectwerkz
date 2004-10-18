909c91ef990ac745a61f6f4b741c6df1f23e2cd9
avasseur <avasseur@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-10-18 13:23:15 +0000

    initial

---
+++ awbench/src/aspectj/awbench/aspectj/MethodExecutionAspect.aj
@@ -0,0 +1,89 @@
+/**************************************************************************************
+ * Copyright (c) Jonas Bon?r, Alexandre Vasseur. All rights reserved.                 *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the LGPL license      *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package awbench.aspectj;
+
+import awbench.method.Execution;
+
+/**
+ * @author <a href="mailto:alex@gnilux.com">Alexandre Vasseur</a>
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public aspect MethodExecutionAspect {
+
+
+    public static int s_count = 0;
+
+
+    before() :
+    execution(* awbench.method.Execution.before()) {
+        s_count++;
+    }
+
+     
+    before() :
+    execution(* awbench.method.Execution.beforeSjp()) {
+        // TODO - we don't make use of sjp / jp so a very lazy impl could hide its weakness
+        // but we want it comparable to before() advice
+        s_count++;
+    }
+
+    before() :
+    execution(* awbench.method.Execution.beforeJp()) {
+        // TODO - we don't make use of sjp / jp so a very lazy impl could hide its weakness
+        // but we want it comparable to before() advice
+    	// f.e. AJ
+    	
+    	// when removing this line, AJ is faster than AW
+    	// else slower but we need to add RTTI in AW to expose the same feature set. 
+    	Object target = thisJoinPoint.getTarget();
+    	
+        s_count++;
+    }
+
+    before(int i) :
+    execution(* awbench.method.Execution.withPrimitiveArgs(int)) && args(i) {
+        int j = i;
+        s_count++;
+    }
+
+    before(Integer i) :
+    execution(* awbench.method.Execution.withWrappedArgs(java.lang.Integer)) && args(i) {
+        Integer j = i;
+        s_count++;
+	}
+
+	before() :
+	execution(* awbench.method.Execution.beforeAfter()) {
+		s_count++;
+	}
+	after() :
+	execution(* awbench.method.Execution.beforeAfter()) {
+		s_count++;
+	}
+	
+	// around gets inlined if thisJoinPoint is not used and thus way faster.
+	Object around() :
+	execution(* awbench.method.Execution.aroundJp()) {
+		s_count++;
+		Object o = thisJoinPointStaticPart.getSignature();
+	    return proceed();
+	}
+
+    //TODO: add Rtti around
+
+	before(int i, Execution t) :
+	execution(* awbench.method.Execution.withArgsAndTarget(int)) && args(i) && target(t) {
+        int j = i;
+        Execution u = t;
+        s_count++;
+    }
+
+	
+}
---
+++ awbench/src/aspectwerkz/awbench/aspectwerkz/MethodExecutionAspect.java
@@ -0,0 +1,81 @@
+/**************************************************************************************
+ * Copyright (c) Jonas Bon?r, Alexandre Vasseur. All rights reserved.                 *
+ * http://aspectwerkz.codehaus.org                                                    *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the LGPL license      *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package awbench.aspectwerkz;
+
+import awbench.method.Execution;
+import org.codehaus.aspectwerkz.joinpoint.StaticJoinPoint;
+import org.codehaus.aspectwerkz.joinpoint.JoinPoint;
+import org.codehaus.aspectwerkz.joinpoint.Rtti;
+
+/**
+ * AW 2.0 aspect
+ * The advice bodies should be the same for comparative benchs that is if the JP is not accessed,
+ * it should not be neither in other frameworks bench.
+ * Else write another test.
+ *
+ * @author <a href="mailto:alex@gnilux.com">Alexandre Vasseur</a>
+ */
+public class MethodExecutionAspect {
+
+    public static int s_count = 0;
+
+
+    /** @Before execution(* awbench.method.Execution.before()) */
+    public void before() {
+        s_count++;
+    }
+
+    /** @Before execution(* awbench.method.Execution.beforeSjp()) */
+    public void beforeSjp(StaticJoinPoint sjp) {
+        // TODO - we don't make use of sjp / jp so a very lazy impl could hide its weakness
+        // but we want it comparable to before() advice
+        s_count++;
+    }
+
+    /** @Before execution(* awbench.method.Execution.beforeJp()) */
+    public void beforeJp(JoinPoint jp) { // , Rtti rtti) {//when using Rtti, the current impl creates a lof of object not lazyly
+        // TODO - we don't make use of sjp / jp so a very lazy impl could hide its weakness
+        // but we want it comparable to before() advice
+        s_count++;
+    }
+
+    /** @Before execution(* awbench.method.Execution.withPrimitiveArgs(int)) && args(i) */
+    public void beforeWithPrimitiveArgs(int i) {
+        int j = i;
+        s_count++;
+    }
+
+    /** @Before execution(* awbench.method.Execution.withWrappedArgs(java.lang.Integer)) && args(i) */
+    public void beforeWithWrappedArgs(Integer i) {
+        Integer j = i;
+        s_count++;
+    }
+
+    /** @Before execution(* awbench.method.Execution.beforeAfter()) */
+    /** @After  execution(* awbench.method.Execution.beforeAfter()) */
+    public void beforeAfter() {
+        s_count++;
+    }
+
+    /** @Around  execution(* awbench.method.Execution.aroundJp()) */
+    public Object aroundJp(JoinPoint jp) throws Throwable {
+        s_count++;
+        return jp.proceed();
+    }
+
+    //TODO: add Rtti around
+
+    /** @Before  execution(* awbench.method.Execution.withArgsAndTarget(int)) && args(i) && target(t) */
+    public void beforeWithArgsAndTarget(int i, Execution t) {
+        int j = i;
+        Execution u = t;
+        s_count++;
+    }
+
+
+}
