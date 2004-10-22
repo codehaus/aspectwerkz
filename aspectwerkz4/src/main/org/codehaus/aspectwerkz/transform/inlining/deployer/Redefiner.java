3e799931bcc311679d7facbfa4c566ff9c44dff0
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-10-22 13:30:35 +0000

    javadoc

--- aspectwerkz4/src/main/org/codehaus/aspectwerkz/transform/inlining/deployer/Redefiner.java
+++ aspectwerkz4/src/main/org/codehaus/aspectwerkz/transform/inlining/deployer/Redefiner.java
@@ -8,7 +8,10 @@
 package org.codehaus.aspectwerkz.transform.inlining.deployer;
 
 /**
- * TODO document
+ * Interface that all "redefiner" implementations should implement.
+ * <p/>
+ * Redefines all classes at all points defined by the <code>ChangeSet</code> passed to the
+ * <code>redefine</code> method.
  *
  * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér </a>
  */
