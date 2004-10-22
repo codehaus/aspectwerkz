30bb39ebc942d2047433dc81a6981f88f1671b19
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-10-22 13:18:34 +0000

    moved hotswapredefininer to extension module, loaded using reflect, remove cyclic deps

--- aspectwerkz4/src/main/org/codehaus/aspectwerkz/transform/inlining/deployer/RedefinerFactory.java
+++ aspectwerkz4/src/main/org/codehaus/aspectwerkz/transform/inlining/deployer/RedefinerFactory.java
@@ -7,12 +7,16 @@
  **************************************************************************************/
 package org.codehaus.aspectwerkz.transform.inlining.deployer;
 
+import org.codehaus.aspectwerkz.ContextClassLoader;
+
 /**
  * Factory for the different redefiner implementations.
  *
  * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér </a>
  */
 public class RedefinerFactory {
+    private static final String HOT_SWAP_REDEFINER_CLASS_NAME =
+            "org.codehaus.aspectwerkz.extension.hotswap.HotSwapRedefiner";
 
     /**
      * Creates a new redefiner instance.
@@ -21,7 +25,15 @@ public class RedefinerFactory {
      */
     public static Redefiner newRedefiner(final Type type) {
         if (type.equals(Type.HOTSWAP)) {
-            return new HotSwapRedefiner();
+            try {
+                Class redefinerClass = ContextClassLoader.loadClass(HOT_SWAP_REDEFINER_CLASS_NAME);
+                return (Redefiner)redefinerClass.newInstance();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException("redefiner class [HotSwapRedefiner] could not be found on classpath, make sure you have the aspectwerkz extensions jar file in your classpath");
+            } catch (Exception e) {
+                throw new RuntimeException("redefiner class [HotSwapRedefiner] could not be instantiated");
+            }
+
         } else if (type.equals(Type.JVMTI)) {
             throw new UnsupportedOperationException("JVMTI is not supported yet");
         } else {
