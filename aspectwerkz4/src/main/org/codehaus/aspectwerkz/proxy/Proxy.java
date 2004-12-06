a5346c75df8e77424ced5267ba8eda33600029b0
jboner <jboner@ad05ba7b-b372-0410-a291-add79d988b4e>
2004-12-06 16:16:25 +0000

    clean up + docs

--- aspectwerkz4/src/main/org/codehaus/aspectwerkz/proxy/Proxy.java
+++ aspectwerkz4/src/main/org/codehaus/aspectwerkz/proxy/Proxy.java
@@ -7,28 +7,28 @@
  **************************************************************************************/
 package org.codehaus.aspectwerkz.proxy;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.InvocationTargetException;
 import java.util.WeakHashMap;
 import java.util.Map;
 
 import org.codehaus.aspectwerkz.hook.impl.ClassPreProcessorHelper;
-import org.codehaus.aspectwerkz.exception.WrappedRuntimeException;
-import org.codehaus.aspectwerkz.util.ContextClassLoader;
+import org.codehaus.aspectwerkz.transform.inlining.AsmHelper;
 
 /**
- * Creates proxy classes from target classes and weaves in all matching aspects deployed in the class loader
+ * Compiles proxy classes from target classes and weaves in all matching aspects deployed in the class loader
  * and defined by the <code>META-INF/aop.xml</code> file.
  *
  * @author <a href="mailto:jboner@codehaus.org">Jonas Bonér </a>
  */
 public class Proxy {
-    public static final String CLASS_LOADER_REFLECT_CLASS_NAME = "java.lang.ClassLoader";
-    public static final String DEFINE_CLASS_METHOD_NAME = "defineClass";
-    public static final String PROXY_SUFFIX_START = "$$ProxiedByAW$$";
-//    private static final RuntimePermission DEFINE_AW_PROXY_CLASS_IN_JAVA_PACKAGE_PERMISSION =
-//            new RuntimePermission("defineAWProxyClassInJavaPackage");
 
+    /**
+     * The suffix for the compiled proxy classes.
+     */
+    public static final String PROXY_SUFFIX_START = "$$ProxiedByAW$$";
+
+    /**
+     * Cache for the compiled proxy classes. Target class is key.
+     */
     private static final Map PROXY_CLASS_CACHE = new WeakHashMap();
 
     /**
@@ -109,8 +109,7 @@ public class Proxy {
         byte[] transformedBytes = ClassPreProcessorHelper.defineClass0Pre(
                 loader, proxyClassName, bytes, 0, bytes.length, null
         );
-        Class proxyClass = defineClass(loader, transformedBytes, proxyClassName);
-        return proxyClass;
+        return AsmHelper.loadClass(loader, transformedBytes, proxyClassName);
     }
 
     /**
@@ -121,74 +120,5 @@ public class Proxy {
      */
     private static String getUniqueClassNameForProxy(final Class clazz) {
         return clazz.getName().replace('.', '/') + PROXY_SUFFIX_START + new Long(Uuid.newUuid()).toString();
-    }
-
-    /**
-     * Adds a class to a class loader and loads it.
-     *
-     * @param loader the class loader
-     * @param bytes  the bytes for the class
-     * @param name   the name of the class
-     * @return the class
-     */
-    private static Class defineClass(ClassLoader loader, final byte[] bytes, final String name) {
-        String className = name.replace('/', '.');
-        try {
-            if (loader == null) {
-                loader = ContextClassLoader.getLoader();
-            }
-
-//            SecurityManager sm = System.getSecurityManager();
-//            if (className != null && className.startsWith("java.") && sm != null) {
-//                sm.checkPermission(DEFINE_AW_PROXY_CLASS_IN_JAVA_PACKAGE_PERMISSION);
-//            }
-
-            Class klass = loader.loadClass(CLASS_LOADER_REFLECT_CLASS_NAME);
-            Method method = klass.getDeclaredMethod(
-                    DEFINE_CLASS_METHOD_NAME, new Class[]{
-                        String.class, byte[].class, int.class, int.class
-                    }
-            );
-
-            method.setAccessible(true);
-            Object[] args = new Object[]{
-                className, bytes, new Integer(0), new Integer(bytes.length)
-            };
-            Class clazz = (Class) method.invoke(loader, args);
-
-            method.setAccessible(false);
-            return clazz;
-
-        } catch (InvocationTargetException e) {
-            if (e.getTargetException() instanceof LinkageError) {
-                Class failoverProxyClass = loadClass(loader, className);
-                if (failoverProxyClass != null) {
-                    return failoverProxyClass;
-                }
-            }
-            throw new WrappedRuntimeException(e.getTargetException());
-        } catch (Exception e) {
-            throw new WrappedRuntimeException(e);
-        }
-    }
-
-    /**
-     * Tries to load a class if unsuccessful returns null.
-     *
-     * @param loader the class loader
-     * @param name   the name of the class
-     * @return the class
-     */
-    private static Class loadClass(ClassLoader loader, final String name) {
-        String className = name.replace('/', '.');
-        try {
-            if (loader == null) {
-                loader = ContextClassLoader.getLoader();
-            }
-            // Use Class.forName since loader.loadClass fails on JBoss UCL
-            return Class.forName(className, false, loader);
-        } catch (Exception e) {
-            return null;
-        }
     }
 }
